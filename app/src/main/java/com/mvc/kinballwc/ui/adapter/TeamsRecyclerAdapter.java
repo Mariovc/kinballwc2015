eb5a6df7a8ffb4be71e6557ac5945e45bda3e214
unknown <mvc74255z@INDRAEXT031998.antena3tv.es>
2015-08-04 10:27:22 +0200

    Teams load image with listener

--- app/src/main/java/com/mvc/kinballwc/ui/adapter/TeamsRecyclerAdapter.java
+++ app/src/main/java/com/mvc/kinballwc/ui/adapter/TeamsRecyclerAdapter.java
@@ -1,6 +1,7 @@
 package com.mvc.kinballwc.ui.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.mvc.kinballwc.R;
 import com.mvc.kinballwc.model.Team;
 
@@ -21,6 +25,7 @@ import java.util.List;
 public class TeamsRecyclerAdapter
         extends RecyclerView.Adapter<TeamsRecyclerAdapter.ViewHolder> {
 
+    private static final String TAG = "TeamsRecyclerAdapter";
     private List<Team> mTeams;
 
     public TeamsRecyclerAdapter(List<Team> teams) {
@@ -69,11 +74,29 @@ public class TeamsRecyclerAdapter
             }
         });
 
-        Glide.with(holder.mImageView.getContext())
-                .load(team.getLogo())
+
+        loadImage(holder.mImageView, team.getLogo());
+    }
+
+    private void loadImage(final ImageView imageView, final String url) {
+        Glide.with(imageView.getContext())
+                .load(url)
                 .placeholder(R.drawable.placeholder)
                 .fitCenter()
-                .into(holder.mImageView);
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        Log.d(TAG, "Glide failed loading image " + url);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        imageView.setImageDrawable(resource);
+                        return false;
+                    }
+                })
+                .into(imageView);
     }
 
     @Override
