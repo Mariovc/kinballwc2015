4d1fcdb2d2b4fd40cae22638a08e778552cbc144
unknown <mvc74255z@INDRAEXT031998.antena3tv.es>
2015-08-05 11:39:58 +0200

    Expand AppBarLayout

--- app/src/main/java/com/mvc/kinballwc/ui/activity/TeamActivity.java
+++ app/src/main/java/com/mvc/kinballwc/ui/activity/TeamActivity.java
@@ -1,26 +1,25 @@
 package com.mvc.kinballwc.ui.activity;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.mvc.kinballwc.R;
-import com.mvc.kinballwc.model.Match;
 import com.mvc.kinballwc.ui.adapter.HeaderAdapter;
-import com.mvc.kinballwc.ui.adapter.MatchRecyclerAdapter;
-
-import java.util.ArrayList;
+import com.mvc.kinballwc.ui.view.ControllableAppBarLayout;
 
 public class TeamActivity extends BaseActivity {
 
     public static final String EXTRA_TEAM_ID = "teamId";
+
+    private ControllableAppBarLayout appBarLayout;
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +49,20 @@ public class TeamActivity extends BaseActivity {
 
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(mLayoutManager);
+
+        appBarLayout = (ControllableAppBarLayout) findViewById(R.id.app_bar_layout);
+        appBarLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appBarLayout.expandToolbar(true);
+            }
+        });
     }
 
     private void loadBackdrop() {
         final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
         Glide.with(this)
-                .load("https://c1.staticflickr.com/3/2583/4161199528_0fbb6f8238_z.jpg")
+                .load("http://pbs.twimg.com/media/Bw7te3iIgAA0qdq.jpg")
 //                .load("http://kin-ball2015.es/pictures/fotos_jugadores/Fotos%20de%20equipo/Naciones/Femenino/Espana.JPG")
                 .centerCrop()
                 .into(imageView);
