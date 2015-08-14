b02a6d3e39d79f594f734ce56cb341d7ca5ec0c9
Mario Velasco Casquero <m3ario@gmail.com>
2015-08-15 01:03:13 +0200

    Updated classification criteria

--- app/src/main/java/com/mvc/kinballwc/ui/fragment/ClassificationTabFragment.java
+++ app/src/main/java/com/mvc/kinballwc/ui/fragment/ClassificationTabFragment.java
@@ -95,6 +95,7 @@ public class ClassificationTabFragment extends Fragment {
             Log.d(TAG, "Activity is destroyed after Parse query");
             return;
         }
+        Collections.sort(matchList, new Match.MatchComparator());
         HashMap<String, Score> scoresMap = new HashMap<>();
         for (Match match : matchList) {
             fillScore(scoresMap, match.getTeam1(), match.getTeam1Points());
@@ -120,6 +121,7 @@ public class ClassificationTabFragment extends Fragment {
             score.setName(team.getName());
         }
         if (matchPoints != null) {
+            // to avoid change classification with semi-final and finals results
             if (score.getMatch3Score() == 0) {
                 if (matchPoints.getMatchPoints() == MatchPoints.MATCH_POINTS_FIRST) {
                     score.setFirstPositionCount(score.getFirstPositionCount() + 1);
@@ -127,6 +129,7 @@ public class ClassificationTabFragment extends Fragment {
                     score.setSecondPositionCount(score.getSecondPositionCount() + 1);
                 }
                 score.setWonPeriodsCount(score.getWonPeriodsCount() + matchPoints.getWonPeriods());
+                score.setFairPlayPoints(score.getFairPlayPoints() + matchPoints.getSportsmanshipPoints());
                 score.setTotalScore(score.getTotalScore() + matchPoints.getTotalPoints());
             }
             if (score.getMatch1Score() == 0) {
